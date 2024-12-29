a52717e89fabd072b53cc100097b7100f37a0a20
dfdrdodm95 <dfdrdodm95@gmail.com>
2024-12-29 19:09:27 +0900

    init project

---
+++ src/main/kotlin/masterplanb/masterplanbbe/MasterPlanBBeApplication.kt
@@ -0,0 +1,11 @@
+package masterplanb.masterplanbbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MasterPlanBBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<MasterPlanBBeApplication>(*args)
+}
---
+++ src/test/kotlin/masterplanb/masterplanbbe/MasterPlanBBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package masterplanb.masterplanbbe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MasterPlanBBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
