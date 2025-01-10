761a6a4763c77dfba5005f1e0ca1a7e40029aab1
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-01-10 11:14:27 +0900

    chore: No Argument constructor 생성을 위한 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("plugin.allopen") version "2.0.0"
     id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.6"
 }
