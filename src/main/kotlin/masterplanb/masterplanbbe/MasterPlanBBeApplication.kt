3b5e872154f48b11b1dbb544413407d04e1e15ee
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-01-16 08:54:53 +0900

    feat: UserFixture 및 동작 확인 메서드 작성

--- src/main/kotlin/masterplanb/masterplanbbe/MasterPlanBBeApplication.kt
+++ src/main/kotlin/masterplanb/masterplanbbe/MasterPlanBBeApplication.kt
@@ -2,8 +2,10 @@ package masterplanb.masterplanbbe
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class MasterPlanBBeApplication
 
 fun main(args: Array<String>) {
