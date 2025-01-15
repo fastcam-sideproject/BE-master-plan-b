4326bb92cbe68bf1e185336975c9dfaa01589ba7
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-01-15 15:46:53 +0900

    chore: jOOQ 의존성 추가:: DSL 생성 확인

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,14 @@
+import dev.monosoul.jooq.RecommendedVersions
+
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    kotlin("plugin.jpa") version "1.9.25"
-    kotlin("plugin.allopen") version "2.0.0"
     id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.6"
+    val kotlinVersion = "2.0.21"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version "1.9.25"
+    kotlin("plugin.allopen") version "2.0.0"
+    id("dev.monosoul.jooq-docker") version "6.1.14"
 }
 
 group = "master-plan-b"
@@ -20,11 +24,80 @@ repositories {
     mavenCentral()
 }
 
+val db_name = findProperty("db.name") as String
+jooq {
+    withContainer {
+        image {
+            name = findProperty("docker.image.name") as String
+            envVars = mapOf(
+                "MYSQL_ROOT_PASSWORD" to findProperty("docker.env.MYSQL_ROOT_PASSWORD") as String,
+                "MYSQL_DATABASE" to findProperty("docker.env.MYSQL_DATABASE") as String
+            )
+        }
+
+        db {
+            username = findProperty("db.username") as String
+            password = findProperty("db.password") as String
+            name = db_name
+            port = (findProperty("db.port") as String).toInt()
+            jdbc {
+                schema = findProperty("db.jdbc.schema") as String
+                driverClassName = findProperty("db.jdbc.driverClassName") as String
+            }
+        }
+    }
+}
+
+tasks {
+    generateJooqClasses {
+        schemas.set(listOf(db_name))
+        outputDirectory.set(project.layout.projectDirectory.dir("src/generated"))
+        includeFlywayTable.set(false)
+
+        usingJavaConfig {
+            generate.apply {
+                isJavaTimeTypes = true
+                isDeprecated = false
+                isDaos = true
+                isFluentSetters = true
+                isRecords = true
+            }
+
+            strategy.name = "jooq.custom.generator.JPrefixGeneratorStrategy"
+
+            database.withForcedTypes(
+                org.jooq.meta.jaxb.ForcedType().apply {
+                    userType = "java.lang.Long"
+                    types = "int unsigned"
+                },
+                org.jooq.meta.jaxb.ForcedType().apply {
+                    userType = "java.lang.Integer"
+                    types = "tinyint unsigned"
+                },
+                org.jooq.meta.jaxb.ForcedType().apply {
+                    userType = "java.lang.Integer"
+                    types = "smallint unsigned"
+                }
+            )
+        }
+    }
+}
+
+sourceSets {
+    main {
+        java {
+            setSrcDirs(listOf("src/main/java", "src/generated"))
+        }
+    }
+}
+
 dependencies {
     // ## Spring Web 및 Jackson 관련 의존성
     implementation("org.springframework.boot:spring-boot-starter-web") // 웹 애플리케이션 개발
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // Kotlin과 Jackson 통합
+    implementation(enforcedPlatform("org.jetbrains.kotlin:kotlin-bom")) // Kotlin BOM
     implementation("org.jetbrains.kotlin:kotlin-reflect") // Kotlin 리플렉션
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8") // Kotlin 표준 라이브러리
     implementation("org.springframework.boot:spring-boot-starter-validation") // 데이터 유효성 검사
 
     // ## Spring Boot 기본 스타터 의존성
@@ -32,17 +105,22 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail") // 이메일 서비스
 
     // ## Spring Security 관련 의존성
-/*
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client") // OAuth2 클라이언트 지원
-    implementation("org.springframework.boot:spring-boot-starter-security") // 보안 관련 기능
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf") // 템플릿 엔진 Thymeleaf
-    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6") // Thymeleaf와 Spring Security 통합
-    testImplementation("org.springframework.security:spring-security-test") // Spring Security 테스트 지원
-*/
+    /*
+        implementation("org.springframework.boot:spring-boot-starter-oauth2-client") // OAuth2 클라이언트 지원
+        implementation("org.springframework.boot:spring-boot-starter-security") // 보안 관련 기능
+        implementation("org.springframework.boot:spring-boot-starter-thymeleaf") // 템플릿 엔진 Thymeleaf
+        implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6") // Thymeleaf와 Spring Security 통합
+        testImplementation("org.springframework.security:spring-security-test") // Spring Security 테스트 지원
+    */
+    // ## jOOQ 및 Flyway 관련 의존성
+    implementation("org.jooq:jooq:${RecommendedVersions.JOOQ_VERSION}")
+    jooqCodegen(project(":jooq-custom"))
+    jooqCodegen("mysql:mysql-connector-java:8.0.29")
+    jooqCodegen("org.flywaydb:flyway-mysql:${RecommendedVersions.FLYWAY_VERSION}")
 
     // ## JWT 관련 의존성
     implementation("io.jsonwebtoken:jjwt:0.9.1") // JWT 토큰 사용
-    
+
     // ## Logging
     implementation("net.logstash.logback:logstash-logback-encoder:7.4")
     implementation("org.springframework.boot:spring-boot-starter-logging:3.1.0")
@@ -76,6 +154,9 @@ dependencies {
     // ## Lombok
     implementation("org.projectlombok:lombok") // 코드 자동 생성을 위한 Lombok
     annotationProcessor("org.projectlombok:lombok") // Lombok 애노테이션 처리기
+
+    // ## jOOQ
+
 
     // ## AWS 통합
     // https://mvnrepository.com/artifact/io.awspring.cloud/spring-cloud-starter-aws
