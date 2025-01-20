8607545b9f41f0dad848d057790ac1d999ae0d97
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-01-20 16:57:06 +0900

    refactor: ErrorResponse의 반환 필드를 ApiResponse와 같이 수정

--- src/main/kotlin/masterplanb/masterplanbbe/common/response/ErrorResponse.kt
+++ src/main/kotlin/masterplanb/masterplanbbe/common/response/ErrorResponse.kt
@@ -5,14 +5,14 @@ import masterplanb.masterplanbbe.common.exception.ErrorCode
 data class ErrorResponse(
     val status: Int,
     val message: String,
-    val code: String
+    val data:Unit? = null
 ) {
     companion object {
         fun of(errorCode: ErrorCode): ErrorResponse {
             return ErrorResponse(
                 status = errorCode.status,
                 message = errorCode.message,
-                code = errorCode.code
+                data = null
             )
         }
     }
