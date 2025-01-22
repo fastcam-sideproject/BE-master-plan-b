e1da8f69d98821c480a5efe6a46fc0bb33c08b4c
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-01-22 09:03:37 +0900

    chore: java 의존성으로 새 init

---
+++ src/test/java/com/example/masterplanbbe/MasterPlanBBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.masterplanbbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterPlanBBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
