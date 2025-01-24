d563eac94260fc1090031452c54dc7b6a0b6e6d6
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-01-24 10:09:55 +0900

    feat: BaseEntity 작성

---
+++ src/main/java/com/example/masterplanbbe/common/domain/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.example.masterplanbbe.common.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @CreatedDate
+        @Column(name = "created_at", updatable = false)
+        @Temporal(TemporalType.TIMESTAMP)
+        private LocalDateTime createdAt;
+
+        @LastModifiedDate
+        @Column(name = "updated_at")
+        @Temporal(TemporalType.TIMESTAMP)
+        private LocalDateTime modifiedAt;
+}
