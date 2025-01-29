5ac8a784b60cadd21916617d7f4d79e8e59a2e92
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-01-29 16:57:17 +0900

    feat: Exam 북마크, 세션 작성 및 CertificationType enum 추가

--- src/main/java/com/example/masterplanbbe/exam/entity/ExamBookmark.java
+++ src/main/java/com/example/masterplanbbe/exam/entity/ExamBookmark.java
@@ -1,4 +1,24 @@
 package com.example.masterplanbbe.exam.entity;
 
-public class ExamBookmark {
+import com.example.masterplanbbe.common.annotation.NonNull;
+import com.example.masterplanbbe.common.domain.BaseEntity;
+import com.example.masterplanbbe.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "exam_bookmark")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ExamBookmark extends BaseEntity {
+    @NonNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @NonNull
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exam_id")
+    private Exam exam;
 }
