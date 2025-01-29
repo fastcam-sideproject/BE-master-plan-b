410b147420bb1d231872c0b9e3b363a764e206c2
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-01-29 16:55:37 +0900

    feat: Exam 리포지토리 및 ExamItemCardDto 작성

--- src/main/java/com/example/masterplanbbe/exam/dto/ExamItemCardDto.java
+++ src/main/java/com/example/masterplanbbe/exam/dto/ExamItemCardDto.java
@@ -1,4 +1,16 @@
 package com.example.masterplanbbe.exam.dto;
 
-public class ExamItemCardDto {
+import com.example.masterplanbbe.exam.entity.Exam;
+import com.example.masterplanbbe.exam.enums.Category;
+
+public record ExamItemCardDto (
+        String title,
+        Category category,
+        Double difficulty,
+        Integer participants,
+        Boolean isBookmarked
+) {
+    public ExamItemCardDto(Exam exam, Boolean isBookmarked) {
+        this(exam.getTitle(), exam.getCategory(), exam.getDifficulty(), exam.getParticipantCount(), isBookmarked);
+    }
 }
--- src/main/java/com/example/masterplanbbe/exam/repository/ExamRepositoryCustom.java
+++ src/main/java/com/example/masterplanbbe/exam/repository/ExamRepositoryCustom.java
@@ -1,4 +1,9 @@
 package com.example.masterplanbbe.exam.repository;
 
+import com.example.masterplanbbe.exam.dto.ExamItemCardDto;
+
+import java.util.List;
+
 public interface ExamRepositoryCustom {
+    List<ExamItemCardDto> getExamItemCards(Long memberId);
 }
