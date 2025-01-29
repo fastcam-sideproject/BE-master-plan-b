880bb53903f43d0da23f3fc0ecef2097f06e65d6
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-01-29 17:03:44 +0900

    feat: Member에 임시 필드 추가

--- src/main/java/com/example/masterplanbbe/exam/entity/ExamSession.java
+++ src/main/java/com/example/masterplanbbe/exam/entity/ExamSession.java
@@ -8,8 +8,8 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDate;
+
 
 @Entity
 @Table(name = "exam_session")
