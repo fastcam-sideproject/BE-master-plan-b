708bee64d7272e4f3630ee2ceccd27f64d83471e
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-01-30 21:48:05 +0900

    feat: getAllExam에 페이지네이션 반영

--- src/main/java/com/example/masterplanbbe/exam/dto/ExamItemCardDto.java
+++ src/main/java/com/example/masterplanbbe/exam/dto/ExamItemCardDto.java
@@ -2,6 +2,7 @@ package com.example.masterplanbbe.exam.dto;
 
 import com.example.masterplanbbe.exam.entity.Exam;
 import com.example.masterplanbbe.exam.enums.Category;
+import com.querydsl.core.annotations.QueryProjection;
 
 public record ExamItemCardDto (
         String title,
@@ -10,6 +11,7 @@ public record ExamItemCardDto (
         Integer participants,
         Boolean isBookmarked
 ) {
+    @QueryProjection
     public ExamItemCardDto(Exam exam, Boolean isBookmarked) {
         this(exam.getTitle(), exam.getCategory(), exam.getDifficulty(), exam.getParticipantCount(), isBookmarked);
     }
