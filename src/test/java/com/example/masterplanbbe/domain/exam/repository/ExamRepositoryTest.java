8ecc2f37a571d25ed614780dc413d79a7ba26f2d
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-01-31 11:51:16 +0900

    feat: ExamBookmark 리포지토리 작성

--- src/test/java/com/example/masterplanbbe/domain/exam/repository/ExamRepositoryTest.java
+++ src/test/java/com/example/masterplanbbe/domain/exam/repository/ExamRepositoryTest.java
@@ -1,15 +1,30 @@
 package com.example.masterplanbbe.domain.exam.repository;
 
+import com.example.masterplanbbe.exam.repository.ExamBookmarkRepository;
 import com.example.masterplanbbe.exam.repository.ExamRepository;
+import com.example.masterplanbbe.member.entity.Member;
+import com.example.masterplanbbe.member.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static com.example.masterplanbbe.domain.fixture.MemberFixture.*;
 
 @SpringBootTest
 public class ExamRepositoryTest {
     @Autowired private ExamRepository examRepository;
+    @Autowired private MemberRepository memberRepository;
+    @Autowired private ExamBookmarkRepository examBookmarkRepository;
+    private Member savedMember;
 
     @BeforeEach
     void setUp() {
+        savedMember = memberRepository.save(createMember());
+    }
+
+    @Test
+    void test() {
+        // test code
     }
 }
