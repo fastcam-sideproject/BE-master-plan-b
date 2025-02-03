fe9144d633dce3155cf95fab6755a705c74ec507
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-02-03 16:11:29 +0900

    feat: ExamService.create 테스트 메서드 추가

--- src/test/java/com/example/masterplanbbe/domain/exam/service/ExamServiceTest.java
+++ src/test/java/com/example/masterplanbbe/domain/exam/service/ExamServiceTest.java
@@ -4,25 +4,32 @@ import com.example.masterplanbbe.domain.exam.dto.ExamItemCardDto;
 import com.example.masterplanbbe.domain.exam.entity.Exam;
 import com.example.masterplanbbe.domain.exam.entity.ExamBookmark;
 import com.example.masterplanbbe.domain.exam.repository.ExamRepositoryPort;
+import com.example.masterplanbbe.domain.exam.request.ExamCreateRequest;
+import com.example.masterplanbbe.domain.exam.response.CreateExamResponse;
 import com.example.masterplanbbe.member.entity.Member;
+import com.example.masterplanbbe.utils.TestUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
 
 import static com.example.masterplanbbe.domain.fixture.ExamFixture.createExam;
+import static com.example.masterplanbbe.domain.fixture.ExamFixture.createExamCreateRequest;
 import static com.example.masterplanbbe.domain.fixture.MemberFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
+import static org.springframework.test.util.ReflectionTestUtils.*;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("시험 서비스 테스트")
@@ -37,13 +44,13 @@ public class ExamServiceTest {
     void get_all_exam() {
         Member member = createMember();
         PageRequest pageRequest = PageRequest.of(0, 5);
-        Page<ExamItemCardDto> mockedExamItemCardPage = createMockedExamItemCardPage(member);
-        given(examRepositoryPort.getExamItemCards(any(Pageable.class), any(String.class))).willReturn(mockedExamItemCardPage);
+        Page<ExamItemCardDto> mocked = createMockedExamItemCardPage(member);
+        given(examRepositoryPort.getExamItemCards(any(Pageable.class), any(String.class))).willReturn(mocked);
 
         examService.getAllExam(pageRequest, "userId");
 
         verify(examRepositoryPort, times(1)).getExamItemCards(any(Pageable.class), any(String.class));
-        assertExamItemCardPage(mockedExamItemCardPage);
+        assertExamItemCardPage(mocked);
     }
 
     private Page<ExamItemCardDto> createMockedExamItemCardPage(Member member) {
@@ -66,7 +73,33 @@ public class ExamServiceTest {
         );
     }
 
-    private boolean isBookmarked(ExamBookmark examBookmark, Exam exam) {
+    private boolean isBookmarked(ExamBookmark examBookmark,
+                                 Exam exam) {
         return examBookmark.getExam() == exam;
+    }
+
+    @Test
+    @DisplayName("관리자는 시험을 추가한다.")
+    void add_exam() {
+        ExamCreateRequest request = createExamCreateRequest("exam1");
+        given(examRepositoryPort.save(any(Exam.class))).willAnswer(this::simulateSavingExamWithId);
+
+        CreateExamResponse result = examService.create(request);
+
+        verify(examRepositoryPort, times(1)).save(any(Exam.class));
+        assertAll(
+                () -> assertThat(result.examId()).isNotNull(),
+                () -> assertThat(result.title()).isEqualTo("exam1"),
+                () -> assertThat(result.category()).isEqualTo(request.category()),
+                () -> assertThat(result.authority()).isEqualTo(request.authority()),
+                () -> assertThat(result.subjects()).isEqualTo(request.subjects())
+        );
+    }
+
+    private Exam simulateSavingExamWithId(InvocationOnMock invocation) {
+        return TestUtils.withSetup(
+                () -> invocation.getArgument(0),
+                exam -> setField(exam, "id", 1L)
+        );
     }
 }
