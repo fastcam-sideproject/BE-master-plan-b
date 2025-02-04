55f93639da0d22b3782dcfc318ae8b3cc01b1670
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-02-04 14:33:21 +0900

    fix: Exam 생성 역직렬화 이슈 수정

--- src/main/java/com/example/masterplanbbe/domain/exam/response/CreateExamResponse.java
+++ src/main/java/com/example/masterplanbbe/domain/exam/response/CreateExamResponse.java
@@ -1,19 +1,28 @@
 package com.example.masterplanbbe.domain.exam.response;
 
+import com.example.masterplanbbe.domain.exam.dto.SubjectDto;
 import com.example.masterplanbbe.domain.exam.entity.Exam;
-import com.example.masterplanbbe.domain.exam.entity.Subject;
 import com.example.masterplanbbe.domain.exam.enums.Category;
 
 import java.util.List;
+import java.util.Objects;
 
 public record CreateExamResponse(
         Long examId,
         String title,
         Category category,
         String authority,
-        List<Subject> subjects
+        List<SubjectDto> subjectDtoList
 ) {
     public CreateExamResponse(Exam exam){
-        this(exam.getId(), exam.getTitle(), exam.getCategory(), exam.getAuthority(), exam.getSubjects());
+        this(
+                exam.getId(),
+                exam.getTitle(),
+                exam.getCategory(),
+                exam.getAuthority(),
+                Objects.requireNonNull(exam.getSubjects()).stream()
+                        .map(SubjectDto::new)
+                        .toList()
+        );
     }
 }
