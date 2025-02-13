e94022045af8624ddbc1b64caf0f0671066149fc
Reddyong <d0209hong@naver.com>
2025-02-13 17:02:34 +0900

    #50 - feat : 회원 시험 일정 리포지토리 생성.
    * JpaRepository 상속하며 JPA 사용할 수 있도록함.

---
+++ src/main/java/com/example/masterplanbbe/domain/userExamSession/repository/UserExamSessionRepository.java
@@ -0,0 +1,7 @@
+package com.example.masterplanbbe.domain.userExamSession.repository;
+
+import com.example.masterplanbbe.domain.userExamSession.entity.UserExamSession;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserExamSessionRepository extends JpaRepository<UserExamSession, Long> {
+}
