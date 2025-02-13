b3207d64ca6f6dea20cf125cbfb8859f76926aaf
Reddyong <d0209hong@naver.com>
2025-02-13 19:31:26 +0900

    #50 - feat : 회원 시험 일정 수정 기능 서비스 로직 구현.
    * 수정할 시험 일정이 존재하는지 판단.
    
    * 수정할 시험 정보 존재하는지 판단.
    
    * 객체 비즈니스 로직으로 수정 진행.

--- src/main/java/com/example/masterplanbbe/domain/userExamSession/service/UserExamSessionService.java
+++ src/main/java/com/example/masterplanbbe/domain/userExamSession/service/UserExamSessionService.java
@@ -2,7 +2,7 @@ package com.example.masterplanbbe.domain.userExamSession.service;
 
 import com.example.masterplanbbe.domain.exam.entity.Exam;
 import com.example.masterplanbbe.domain.exam.repository.ExamRepositoryPort;
-import com.example.masterplanbbe.domain.userExamSession.dto.request.UserExamSessionCreateRequest;
+import com.example.masterplanbbe.domain.userExamSession.dto.request.UserExamSessionRequest;
 import com.example.masterplanbbe.domain.userExamSession.dto.response.UserExamSessionResponse;
 import com.example.masterplanbbe.domain.userExamSession.entity.UserExamSession;
 import com.example.masterplanbbe.domain.userExamSession.repository.UserExamSessionRepositoryPort;
@@ -21,12 +21,23 @@ public class UserExamSessionService {
     private final UserExamSessionRepositoryPort userExamSessionRepositoryPort;
 
     @Transactional
-    public UserExamSessionResponse create(UserExamSessionCreateRequest request, Long memberId) {
+    public UserExamSessionResponse create(UserExamSessionRequest request, Long memberId) {
         Exam exam = examRepositoryPort.getById(request.examId());
 
         Member member = memberRepositoryPort.findById(memberId);
 
         UserExamSession userExamSession = userExamSessionRepositoryPort.save(UserExamSession.of(exam, member, request.date(), request.startTime(), request.endTime()));
+
+        return UserExamSessionResponse.from(userExamSession);
+    }
+
+    @Transactional
+    public UserExamSessionResponse update(UserExamSessionRequest request, Long updateId) {
+        UserExamSession userExamSession = userExamSessionRepositoryPort.getById(updateId);
+
+        Exam exam = examRepositoryPort.getById(request.examId());
+
+        userExamSession.updateUserExamSession(request, exam);
 
         return UserExamSessionResponse.from(userExamSession);
     }
