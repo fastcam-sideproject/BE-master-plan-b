acea246d7d0b7927bfafa65958ba2dd2503aa00d
Reddyong <d0209hong@naver.com>
2025-02-14 15:34:23 +0900

    #50 - feat : 회원 시험 일정 기능에 필요한 조회 쿼리 메서드 추가.
    * 나의 시험 일정 id와 나의 id 값을 통해 조회하는 쿼리 메서드.

--- src/main/java/com/example/masterplanbbe/domain/userExamSession/repository/UserExamSessionRepository.java
+++ src/main/java/com/example/masterplanbbe/domain/userExamSession/repository/UserExamSessionRepository.java
@@ -3,5 +3,8 @@ package com.example.masterplanbbe.domain.userExamSession.repository;
 import com.example.masterplanbbe.domain.userExamSession.entity.UserExamSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserExamSessionRepository extends JpaRepository<UserExamSession, Long> {
+    Optional<UserExamSession> findByIdAndMemberId(Long id, Long memberId);
 }
