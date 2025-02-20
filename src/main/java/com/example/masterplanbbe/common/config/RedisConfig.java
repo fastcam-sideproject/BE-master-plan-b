e3cf7dfb230eb0d2360422b7d0847749d7802a03
Jeong TaeHee <th1563@naver.com>
2025-02-20 17:25:45 +0900

    feat: 채팅 기능을 위한 Redis 설정 추가

--- src/main/java/com/example/masterplanbbe/common/config/RedisConfig.java
+++ src/main/java/com/example/masterplanbbe/common/config/RedisConfig.java
@@ -1,5 +1,9 @@
 package com.example.masterplanbbe.common.config;
 
+import com.example.masterplanbbe.chat.service.RedisSubscriber;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.lettuce.core.ClientOptions;
 import io.lettuce.core.SocketOptions;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,9 +14,13 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.time.Duration;
 
@@ -58,5 +66,61 @@ public class RedisConfig {
         redisTemplate.setHashValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
 
         return redisTemplate;
+    }
+
+    //=======================Chat==============================//
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule()); // LocalDateTime 지원
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // Timestamp 형식 방지
+        return objectMapper;
+    }
+
+    @Bean(name = "chatTemplate")
+    public RedisTemplate<String, Object> chatRedisTemplate(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        // Jackson을 이용한 직렬화 설정
+        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
+
+        template.setKeySerializer(new StringRedisSerializer()); // 키 한글 깨짐 방지
+        template.setValueSerializer(serializer); // 객체 직렬화 (JSON)
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        return template;
+    }
+
+    // RedisSubscriber 메시지 리스너 어댑터
+    @Bean
+    public MessageListenerAdapter listenerAdapter(RedisSubscriber redisSubscriber) {
+        return new MessageListenerAdapter(redisSubscriber, "onMessage");
+    }
+
+    // Redis Pub/Sub 메시지 리스너 설정
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            RedisConnectionFactory redisConnectionFactory, MessageListenerAdapter listenerAdapter) {
+
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(listenerAdapter, new PatternTopic("chat:*"));
+        container.setTaskExecutor(redisTaskExecutor()); // 멀티스레드 실행 설정
+        return container;
+    }
+
+    // Redis 메시지 처리를 위한 비동기 TaskExecutor 설정
+    @Bean
+    public ThreadPoolTaskExecutor redisTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);   // 기본적으로 실행할 스레드 개수
+        executor.setMaxPoolSize(10);    // 최대 스레드 개수 (CPU 과부화를 막기위해 코어 * 2)
+        executor.setQueueCapacity(50);  // 대기열 크기
+        executor.setThreadNamePrefix("RedisExecutor-");
+        executor.initialize();
+        return executor;
     }
 }
