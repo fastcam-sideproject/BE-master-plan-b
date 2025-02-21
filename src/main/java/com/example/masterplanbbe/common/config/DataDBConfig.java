2cdae059f4ee035785f4d77a64c53dc0570c1a48
kimD0ngjun <chickentasty0112@gmail.com>
2025-02-22 03:05:29 +0900

    feat: 데이터베이스 이원화 설정 클래스 적용

---
+++ src/main/java/com/example/masterplanbbe/common/config/DataDBConfig.java
@@ -0,0 +1,86 @@
+package com.example.masterplanbbe.common.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = {
+                "com.example.masterplanbbe.domain.comment.repository",
+                "com.example.masterplanbbe.domain.exam.repository",
+                "com.example.masterplanbbe.domain.examBookmark.repository",
+                "com.example.masterplanbbe.domain.member.repository",
+                "com.example.masterplanbbe.domain.post.repository",
+                "com.example.masterplanbbe.domain.studyLog.repository",
+                "com.example.masterplanbbe.domain.userExamSession.repository"
+        },
+        entityManagerFactoryRef = "dataEntityManager",
+        transactionManagerRef = "dataTransactionManager"
+)
+public class DataDBConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource-service")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean dataEntityManager() {
+
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setPackagesToScan(
+                "com.example.masterplanbbe.domain.comment.entity",
+                "com.example.masterplanbbe.domain.exam.entity",
+                "com.example.masterplanbbe.domain.examBookmark.entity",
+                "com.example.masterplanbbe.domain.examSession.entity",
+                "com.example.masterplanbbe.domain.member.entity",
+                "com.example.masterplanbbe.domain.post.entity",
+                "com.example.masterplanbbe.domain.studyLog.entity",
+                "com.example.masterplanbbe.domain.userExamSession.entity"
+        );
+        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", "update");
+//        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+
+
+        factoryBean.setJpaPropertyMap(properties);
+        return factoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager dataTransactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(dataEntityManager().getObject());
+
+        return transactionManager;
+    }
+
+    @Bean
+    public PlatformTransactionManager jdbcTransactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
