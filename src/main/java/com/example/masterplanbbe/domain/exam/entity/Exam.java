18056394927619d075312045731910ee72a7cf5c
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-02-24 08:56:24 +0900

    refactor: 시험 상세 정보 엔티티, ExamDetail 작성

--- src/main/java/com/example/masterplanbbe/domain/exam/entity/Exam.java
+++ src/main/java/com/example/masterplanbbe/domain/exam/entity/Exam.java
@@ -1,7 +1,6 @@
 package com.example.masterplanbbe.domain.exam.entity;
 
 import com.example.masterplanbbe.common.domain.FullAuditEntity;
-import com.example.masterplanbbe.domain.exam.enums.CertificationType;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -10,28 +9,17 @@ import lombok.*;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Exam extends FullAuditEntity {
+    @OneToOne
+    @JoinColumn(name = "exam_detail_id")
+    private ExamDetail examDetail;
+
     @Column(nullable = false)
     private String name;
-
-    @Enumerated(EnumType.STRING)
-    private CertificationType certificationType;
 
     @Column(nullable = false)
     private Double difficulty;
 
     @Column(nullable = false)
     private Integer participantCount;
-
-    @Column
-    private String preparation;
-
-    @Column
-    private String eligibility;
-
-    @Column
-    private String examStructure;
-
-    @Column
-    private String passingCriteria;
 
 }
---
+++ src/main/java/com/example/masterplanbbe/domain/exam/entity/ExamDetail.java
@@ -0,0 +1,30 @@
+package com.example.masterplanbbe.domain.exam.entity;
+
+import com.example.masterplanbbe.common.domain.FullAuditEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "exam_details")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ExamDetail extends FullAuditEntity {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "spec_id")
+    private Spec spec;
+
+    @Column(nullable = false)
+    private String preparation;
+
+    @Column(nullable = false)
+    private String eligibility;
+
+    @Column(nullable = false)
+    private String examStructure;
+
+    @Column(nullable = false)
+    private String passingCriteria;
+
+}
