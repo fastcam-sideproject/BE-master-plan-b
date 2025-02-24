958a00a5a4675a2f60716c6bc3b2773e076bedac
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-02-24 18:12:24 +0900

    refactor: 스펙 목록 조회용 DTO 작성

---
+++ src/main/java/com/example/masterplanbbe/domain/exam/dto/SpecItemCardDto.java
@@ -0,0 +1,17 @@
+package com.example.masterplanbbe.domain.exam.dto;
+
+import com.example.masterplanbbe.domain.exam.enums.Category;
+
+import java.time.LocalDate;
+
+public record SpecItemCardDto(
+        String name,
+        Category category,
+        Double difficulty,
+        Integer participants,
+        Boolean isBookmarked,
+        LocalDate applyStartDate,
+        LocalDate applyEndDate,
+        LocalDate examStartDate
+) {
+}
