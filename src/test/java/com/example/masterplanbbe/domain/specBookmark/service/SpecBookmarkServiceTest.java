14e74d9b4cb1d51b5c6fe8659745530cc57e8848
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-02-25 20:50:17 +0900

    test: SpecBookmark 서비스 테스트 작성

--- src/test/java/com/example/masterplanbbe/domain/specBookmark/service/SpecBookmarkServiceTest.java
+++ src/test/java/com/example/masterplanbbe/domain/specBookmark/service/SpecBookmarkServiceTest.java
@@ -1,9 +1,13 @@
 package com.example.masterplanbbe.domain.specBookmark.service;
 
+import com.example.masterplanbbe.domain.spec.entity.Spec;
 import com.example.masterplanbbe.domain.spec.repository.SpecRepositoryPort;
+import com.example.masterplanbbe.domain.specBookmark.entity.SpecBookmark;
 import com.example.masterplanbbe.domain.specBookmark.repository.SpecBookmarkRepository;
+import com.example.masterplanbbe.domain.specBookmark.response.CreateSpecBookmarkResponse;
 import com.example.masterplanbbe.member.entity.Member;
 import com.example.masterplanbbe.member.repository.MemberRepositoryPort;
+import com.example.masterplanbbe.utils.TestUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,11 +15,19 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static com.example.masterplanbbe.domain.fixture.MemberFixture.getExistingMember;
+import static com.example.masterplanbbe.domain.fixture.SpecFixture.getExistingSpec;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.springframework.test.util.ReflectionTestUtils.setField;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-@DisplayName("시험 북마크 서비스 테스트")
+@DisplayName("스펙 북마크 서비스 테스트")
 public class SpecBookmarkServiceTest {
     @InjectMocks
     SpecBookmarkService specBookmarkService;
@@ -27,53 +39,35 @@ public class SpecBookmarkServiceTest {
     SpecRepositoryPort specRepositoryPort;
 
     @Test
-    @DisplayName("사용자는 시험 북마크를 추가한다.")
-    void add_exam_bookmark() {
+    @DisplayName("사용자는 스펙 북마크를 추가한다.")
+    void add_spec_bookmark() {
         Member member = getExistingMember();
-        Exam exam = getExistingExam();
+        Spec spec = getExistingSpec();
         given(memberRepositoryPort.findById(anyLong())).willReturn(member);
-        given(examRepositoryPort.getById(anyLong())).willReturn(exam);
-        given(examBookmarkRepository.save(any(ExamBookmark.class))).willAnswer(this::simulateSavingExamBookmark);
+        given(specRepositoryPort.getById(anyLong())).willReturn(spec);
+        given(specBookmarkRepository.save(any(SpecBookmark.class))).willAnswer(TestUtils::simulateSavingEntity);
 
-        CreateExamBookmarkResponse result = examBookmarkService.createExamBookmark(member.getId(), exam.getId());
+        CreateSpecBookmarkResponse result = specBookmarkService.createExamBookmark(member.getId(), spec.getId());
 
         verify(memberRepositoryPort, times(1)).findById(anyLong());
-        verify(examRepositoryPort, times(1)).getById(anyLong());
-        verify(examBookmarkRepository, times(1)).save(any(ExamBookmark.class));
+        verify(specRepositoryPort, times(1)).getById(anyLong());
+        verify(specBookmarkRepository, times(1)).save(any(SpecBookmark.class));
         assertAll(
                 () -> assertThat(result.examBookmarkId()).isNotNull(),
                 () -> assertThat(result.memberId()).isEqualTo(member.getId()),
-                () -> assertThat(result.examId()).isEqualTo(exam.getId())
+                () -> assertThat(result.specId()).isEqualTo(spec.getId())
         );
     }
-
-    private ExamBookmark simulateSavingExamBookmark(InvocationOnMock invocation) {
-        return withSetup(
-                () -> invocation.getArgument(0),
-                    examBookmark -> setField(examBookmark, "id", 1L)
-        );
-    }
-
-    private Member getExistingMember() {
-        return withSetup(MemberFixture::createMember, instance -> setField(instance, "id", 1L));
-    }
-
 
     @Test
-    @DisplayName("사용자는 시험 북마크를 삭제한다.")
-    void delete_exam_bookmark() {
+    @DisplayName("사용자는 스펙 북마크를 삭제한다.")
+    void delete_spec_bookmark() {
         Long examBookmarkId = 1L;
-        willDoNothing().given(examBookmarkRepository).deleteById(examBookmarkId);
+        willDoNothing().given(specRepositoryPort).deleteById(examBookmarkId);
 
-        examBookmarkService.deleteExamBookmark(examBookmarkId);
+        specBookmarkService.deleteExamBookmark(examBookmarkId);
 
-        verify(examBookmarkRepository, times(1)).deleteById(anyLong());
+        verify(specBookmarkRepository, times(1)).deleteById(anyLong());
     }
 
-    private Spec getExistingSpec() {
-        return TestUtils.getExistingEntity(() -> createSpec("Spec1"));
-    }
-    private Exam getExistingExam() {
-        return withSetup(() -> createExam("exam1"), instance -> setField(instance, "id", 1L));
-    }
 }
