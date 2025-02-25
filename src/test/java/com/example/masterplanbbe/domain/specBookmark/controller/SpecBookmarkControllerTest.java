880c310b6b8a12230f58e92ce222c7031373b04a
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-02-26 00:51:37 +0900

    test: SpecBookmark 컨트롤러 테스트 작성

--- src/test/java/com/example/masterplanbbe/domain/specBookmark/controller/SpecBookmarkControllerTest.java
+++ src/test/java/com/example/masterplanbbe/domain/specBookmark/controller/SpecBookmarkControllerTest.java
@@ -1,6 +1,9 @@
 package com.example.masterplanbbe.domain.specBookmark.controller;
 
+import com.example.masterplanbbe.common.response.ApiResponse;
+import com.example.masterplanbbe.domain.specBookmark.response.CreateSpecBookmarkResponse;
 import com.example.masterplanbbe.domain.specBookmark.service.SpecBookmarkService;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,14 +15,21 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static com.example.masterplanbbe.domain.fixture.SpecBookmarkFixture.*;
+import static java.nio.charset.StandardCharsets.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.springframework.test.util.ReflectionTestUtils.setField;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("스펙 북마크 컨트롤러 테스트")
 @ExtendWith(MockitoExtension.class)
@@ -42,21 +52,14 @@ public class SpecBookmarkControllerTest {
     }
 
     @Test
-    @DisplayName("사용자는 시험을 북마크한다.")
-    void create_exam_bookmark() throws Exception {
+    @DisplayName("사용자는 스펙을 북마크한다.")
+    void create_spec_bookmark() throws Exception {
         Long memberId = 1L;
-        Long examId = 1L;
-        Member member = createExistingMemberFrom(memberId);
-        Exam exam = createExistingExamFrom(examId);
-        CreateExamBookmarkResponse mockedResult = new CreateExamBookmarkResponse(
-                createExistingExamBookmarkOf(
-                    member,
-                    exam
-                )
-        );
-        given(examBookmarkService.createExamBookmark(examId, memberId)).willReturn(mockedResult);
+        Long specId = 1L;
+        CreateSpecBookmarkResponse mockedResult = new CreateSpecBookmarkResponse(createExistingSpecBookmarkOf(memberId, specId));
+        given(specBookmarkService.createExamBookmark(any(Long.class), any(Long.class))).willReturn(mockedResult);
 
-        ResultActions resultActions = mockMvc.perform(post("/api/v1/spec/{specId}/bookmark", examId)
+        ResultActions resultActions = mockMvc.perform(post("/api/v1/spec/{specId}/bookmark", specId)
                 .contentType(MediaType.APPLICATION_JSON)
                 .param("memberId", memberId.toString())
                 .accept(MediaType.APPLICATION_JSON));
@@ -65,31 +68,24 @@ public class SpecBookmarkControllerTest {
                 .andDo(print())
                 .andDo(result -> {
                     String responseContent = result.getResponse().getContentAsString();
-                    ApiResponse<CreateExamBookmarkResponse> response = objectMapper.readValue(responseContent, new TypeReference<>() {
+                    ApiResponse<CreateSpecBookmarkResponse> response = objectMapper.readValue(responseContent, new TypeReference<>() {
                     });
-                    CreateExamBookmarkResponse data = response.getData();
+                    CreateSpecBookmarkResponse data = response.getData();
                     assertAll(
                             () -> assertThat(data.examBookmarkId()).isNotNull(),
                             () -> assertThat(data.memberId()).isEqualTo(memberId),
-                            () -> assertThat(data.examId()).isEqualTo(examId)
+                            () -> assertThat(data.specId()).isEqualTo(specId)
                     );
                 });
     }
 
-    private ExamBookmark createExistingExamBookmarkOf(Member member,
-                                                      Exam exam) {
-        return TestUtils.withSetup(() -> new ExamBookmark(member, exam), examBookmark -> {
-            setField(examBookmark, "id", 1L);
-        });
-    }
-
     @Test
-    @DisplayName("사용자는 시험 북마크를 삭제한다.")
-    void delete_exam_bookmark() throws Exception {
-        Long examId = 1L;
-        doNothing().when(examBookmarkService).deleteExamBookmark(examId);
+    @DisplayName("사용자는 스펙 북마크를 삭제한다.")
+    void delete_spec_bookmark() throws Exception {
+        Long specId = 1L;
+        doNothing().when(specBookmarkService).deleteExamBookmark(specId);
 
-        ResultActions resultActions = mockMvc.perform(delete("/api/v1/exam/{examId}/bookmark", examId)
+        ResultActions resultActions = mockMvc.perform(delete("/api/v1/spec/{specId}/bookmark", specId)
                 .characterEncoding(UTF_8));
 
         resultActions.andExpectAll(status().isOk(), content().contentType(MediaType.APPLICATION_JSON))
@@ -98,7 +94,7 @@ public class SpecBookmarkControllerTest {
                     String responseContent = result.getResponse().getContentAsString(UTF_8);
                     ApiResponse<String> response = objectMapper.readValue(responseContent, new TypeReference<>() {
                     });
-                    assertThat(response.getData()).isEqualTo("시험 북마크 삭제 성공");
+                    assertThat(response.getData()).isEqualTo("스펙 북마크 삭제 성공");
                 });
     }
 }
