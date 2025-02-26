fbe1955c8ac1e0215acb80767ec180a5250b59ad
Reddyong <d0209hong@naver.com>
2025-02-26 16:29:36 +0900

    #74 - 학습 기록 엔티티 요구사항에 맞게 수정.
    * 회원과 시험 엔티티와 일대다 연관관계 매핑.
    
    * 학습을 기록한 날짜, 학습 시간을 분으로 정보 가짐.
    
    * 학습 기록 객체 생성을 위한 of 메서드 생성 - 입력으로 시간과 분이 들어오는 것을 반영하여 분으로 변경후 생성.

--- src/main/java/com/example/masterplanbbe/domain/studyLog/entity/StudyLog.java
+++ src/main/java/com/example/masterplanbbe/domain/studyLog/entity/StudyLog.java
@@ -1,9 +1,9 @@
 package com.example.masterplanbbe.domain.studyLog.entity;
 
-import com.example.masterplanbbe.common.annotation.NonNull;
-import com.example.masterplanbbe.common.annotation.Nullable;
 import com.example.masterplanbbe.common.domain.FullAuditEntity;
+import com.example.masterplanbbe.domain.exam.entity.Exam;
 import com.example.masterplanbbe.domain.studyLog.enums.InputSource;
+import com.example.masterplanbbe.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -13,38 +13,36 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "study_log")
+@Table(name = "study_logs")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StudyLog extends FullAuditEntity {
-    @NonNull
-    @Column
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exam_id", nullable = false)
+    private Exam exam;
+
+    @Column(name = "date", nullable = false)
     private LocalDate studyDate;
 
-/*
-    @ManyToOne
-    @JoinColumn(name = "exam_id")
-    private Exam exam;
-*/
+    @Column(name = "total_study_times", nullable = false)
+    private Integer totalStudyTimes;
 
-    @Nullable
-    @Column
-    private String subject;
-
-    @Nullable
-    @Column
-    private Integer elapsedTime;
-
-    @Column
-    private String title;
-
-    @Column
+    @Column(name = "content", nullable = false)
     private String content;
 
-    @NonNull
-    @Column
+    @Column(name = "input_source", nullable = false)
     @Enumerated(EnumType.STRING)
     private InputSource inputSource;
+
+    public static StudyLog of(Member member, Exam exam, LocalDate studyDate, Integer hour, Integer minutes, String content, InputSource inputSource) {
+        Integer totalStudyTimes = hour * 60 + minutes;
+        return new StudyLog(member, exam, studyDate, totalStudyTimes, content, inputSource);
+    }
 }
 
