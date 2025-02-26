295aacd67e78c168423d3b00b149369d4ca1ac37
Reddyong <d0209hong@naver.com>
2025-02-26 16:30:58 +0900

    #74 - 학습 기록 생성 컨트롤러 구현.
    * 시험 id, 학습 기록 날짜, 학습 시간/분, 학습 기록 내용, 작성 타입(컴퓨터, 모바일) 정보 요청으로 받음.

--- src/main/java/com/example/masterplanbbe/domain/studyLog/controller/StudyLogController.java
+++ src/main/java/com/example/masterplanbbe/domain/studyLog/controller/StudyLogController.java
@@ -1,13 +1,15 @@
 package com.example.masterplanbbe.domain.studyLog.controller;
 
 import com.example.masterplanbbe.common.response.ApiResponse;
-import com.example.masterplanbbe.domain.studyLog.request.CreateStudyLogRequest;
-import com.example.masterplanbbe.domain.studyLog.response.CreateStudyLogResponse;
+import com.example.masterplanbbe.domain.studyLog.request.StudyLogRequest;
 import com.example.masterplanbbe.domain.studyLog.service.StudyLogService;
+import com.example.masterplanbbe.security.user.UserDetailsImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "StudyLog controller api", description = "공부기록 API")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/study-log")
+@RequestMapping("/api/v1/study-logs")
 public class StudyLogController {
     private final StudyLogService studyLogService;
 
-    @Operation(summary = "공부기록 생성")
-    public ResponseEntity<ApiResponse<CreateStudyLogResponse>> createStudyLog(
-            @RequestBody CreateStudyLogRequest request
+    @Operation(summary = "학습 기록 작성")
+    @PostMapping(path = "")
+    public ResponseEntity<ApiResponse<?>> create(
+            @RequestBody StudyLogRequest studyLogRequest,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
         return ResponseEntity.ok()
-                .body(ApiResponse.ok(studyLogService.createStudyLog(request)));
+                .body(ApiResponse.ok(studyLogService.create(studyLogRequest, userDetails.getUsername())));
     }
+
 }
