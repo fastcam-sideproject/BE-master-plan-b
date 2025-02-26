bfbc6e101145c6c3946c4afe863f84f5994cc073
Reddyong <d0209hong@naver.com>
2025-02-26 16:52:50 +0900

    #74 - refactor : 스웨거에서 enum 타입 설명 및 조건 추가.

--- src/main/java/com/example/masterplanbbe/domain/studyLog/request/StudyLogRequest.java
+++ src/main/java/com/example/masterplanbbe/domain/studyLog/request/StudyLogRequest.java
@@ -1,6 +1,7 @@
 package com.example.masterplanbbe.domain.studyLog.request;
 
 import com.example.masterplanbbe.domain.studyLog.enums.InputSource;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDate;
 
@@ -10,6 +11,7 @@ public record StudyLogRequest(
         Integer hour,
         Integer minute,
         String content,
+        @Schema(description = "검색 타입 (COMPUTER, MOBILE)", example = "COMPUTER", allowableValues = {"COMPUTER", "MOBILE"})
         InputSource inputSource
 ) {
 }
