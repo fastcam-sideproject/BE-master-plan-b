6813f5b8e5450cd4db93de02101640ebfc7c1924
Reddyong <d0209hong@naver.com>
2025-02-26 16:35:02 +0900

    #74 - 학습 기록 생성 서비스 로직 구현.
    * 회원과 시험의 정보 검증 후 조회.
    
    * 학습 기록 생성하여 저장 후 응답 반환.

--- src/main/java/com/example/masterplanbbe/domain/studyLog/service/StudyLogService.java
+++ src/main/java/com/example/masterplanbbe/domain/studyLog/service/StudyLogService.java
@@ -1,17 +1,33 @@
 package com.example.masterplanbbe.domain.studyLog.service;
 
-import com.example.masterplanbbe.domain.studyLog.repository.StudyLogRepository;
-import com.example.masterplanbbe.domain.studyLog.request.CreateStudyLogRequest;
-import com.example.masterplanbbe.domain.studyLog.response.CreateStudyLogResponse;
+import com.example.masterplanbbe.domain.exam.entity.Exam;
+import com.example.masterplanbbe.domain.exam.repository.ExamRepositoryPort;
+import com.example.masterplanbbe.domain.studyLog.entity.StudyLog;
+import com.example.masterplanbbe.domain.studyLog.repository.StudyLogRepositoryPort;
+import com.example.masterplanbbe.domain.studyLog.request.StudyLogRequest;
+import com.example.masterplanbbe.domain.studyLog.response.StudyLogResponse;
+import com.example.masterplanbbe.member.entity.Member;
+import com.example.masterplanbbe.member.repository.MemberRepositoryPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class StudyLogService {
-    private final StudyLogRepository studyLogRepository;
+    private final MemberRepositoryPort memberRepositoryPort;
+    private final StudyLogRepositoryPort studyLogRepositoryPort;
+    private final ExamRepositoryPort examRepositoryPort;
 
-    public CreateStudyLogResponse createStudyLog(CreateStudyLogRequest request) {
-        return new CreateStudyLogResponse(studyLogRepository.save(request.toEntity()));
+    @Transactional
+    public StudyLogResponse create(StudyLogRequest studyLogRequest, String memberId) {
+
+        Member member = memberRepositoryPort.findByUserId(memberId);
+
+        Exam exam = examRepositoryPort.getById(studyLogRequest.examId());
+
+        StudyLog studyLog = studyLogRepositoryPort.save(StudyLog.of(member, exam, studyLogRequest.studyDate(), studyLogRequest.hour(), studyLogRequest.minute(), studyLogRequest.content(), studyLogRequest.inputSource()));
+
+        return StudyLogResponse.from(studyLog);
     }
 }
