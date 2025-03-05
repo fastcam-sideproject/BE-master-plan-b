315ae9e94e593e651927cf9e37d5110a2094c085
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-03-05 10:15:33 +0900

    feat: PageableExecutionUtils의 parameter를 조정한 유틸 클래스 작성

--- src/main/java/com/example/masterplanbbe/common/util/CustomPageUtils.java
+++ src/main/java/com/example/masterplanbbe/common/util/CustomPageUtils.java
@@ -1,4 +1,30 @@
 package com.example.masterplanbbe.common.util;
 
-public class CustomPageUtils {
+import com.example.masterplanbbe.common.page.CustomPage;
+import com.example.masterplanbbe.common.request.CustomPageRequest;
+import io.jsonwebtoken.lang.Assert;
+
+import java.util.List;
+import java.util.function.LongSupplier;
+
+public abstract class CustomPageUtils {
+    private CustomPageUtils() {
+    }
+
+    public static <T> CustomPage<T> getPage(List<T> content, CustomPageRequest pageRequest, LongSupplier totalSupplier) {
+        Assert.notNull(content, "content must not be null");
+        Assert.notNull(totalSupplier, "totalSupplier must not be null");
+        if (pageRequest.getOffset()  == 0) {
+            if (pageRequest.size() > content.size()) {
+                return new CustomPage<>(0, pageRequest.size(), content.size(), content);
+            }
+            return new CustomPage<>(0, pageRequest.size(), totalSupplier.getAsLong(), content);
+        }
+
+        if (!content.isEmpty() && pageRequest.size() > content.size()) {
+            return new CustomPage<>(pageRequest.page(), pageRequest.size(), pageRequest.getOffset() + content.size(), content);
+        }
+
+        return new CustomPage<>(pageRequest.page(), pageRequest.size(), totalSupplier.getAsLong(), content);
+    }
 }
