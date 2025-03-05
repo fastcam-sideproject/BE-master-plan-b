222794fa004c9bc0f884a41b0ce2b6cb96ef8739
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-03-05 10:13:32 +0900

    feat: Pageable과 호환되지 않는, Page 관련 객체 CustomPage 작성

--- src/main/java/com/example/masterplanbbe/common/page/CustomPage.java
+++ src/main/java/com/example/masterplanbbe/common/page/CustomPage.java
@@ -1,4 +1,20 @@
 package com.example.masterplanbbe.common.page;
 
-public class CustomPage {
+import java.util.List;
+
+public record CustomPage<T>(
+        int page,
+        int size,
+        long totalElements,
+        List<T> content
+) {
+
+    public boolean hasNext() {
+        return getOffset() + content.size() < totalElements;
+    }
+
+    private int getOffset() {
+        return page * size;
+    }
+
 }
