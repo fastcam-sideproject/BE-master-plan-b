c4adc428d4e8dd83dd9883baa4a8d95b12ed142e
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-03-07 20:15:39 +0900

    refactor: 목록 조회 쿼리용 DTO의 프로젝션 생성자를 변경

--- src/main/java/com/example/masterplanbbe/domain/spec/repository/SpecRepositoryAdapter.java
+++ src/main/java/com/example/masterplanbbe/domain/spec/repository/SpecRepositoryAdapter.java
@@ -11,13 +11,10 @@ import com.example.masterplanbbe.domain.spec.dto.SpecWithDetailsDto;
 import com.example.masterplanbbe.domain.spec.entity.Spec;
 import com.example.masterplanbbe.domain.spec.enums.SpecSortOption;
 import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.jpa.JPAExpressions;
-import com.querydsl.jpa.JPQLSubQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.LongSupplier;
@@ -38,19 +35,6 @@ public class SpecRepositoryAdapter implements SpecRepositoryPort, SpecRepository
     @Override
     public CustomPage<SpecItemCardDto> getSpecItemCards(CustomPageRequest<SpecSortOption> request,
                                                         Long memberId) {
-        LocalDate today = LocalDate.now();
-
-        JPQLSubQuery<Long> closestExamIdSubquery = JPAExpressions
-                .select(exam.id)
-                .from(exam)
-                .join(exam.examDetail, examDetail)
-                .where(
-                        examDetail.spec.id.eq(spec.id)
-                                .and(exam.applyEndDate.goe(today))
-                )
-                .orderBy(exam.examStartDate.asc())
-                .limit(1);
-
         OrderSpecifier<?> orderSpecifier = request.sort() != null ?
                 SortUtil.getOrderSpecifier(request.sort(), request.isAsc()) :
                 spec.createdAt.asc();
@@ -65,8 +49,8 @@ public class SpecRepositoryAdapter implements SpecRepositoryPort, SpecRepository
                 .leftJoin(specBookmark)
                 .on(specBookmark.spec.id.eq(spec.id).and(specBookmark.member.id.eq(memberId)))
                 .leftJoin(exam)
+                .on(exam.id.eq(spec.latestExam.id))
                 .orderBy(orderSpecifier)
-                .on(exam.id.eq(closestExamIdSubquery))
                 .offset(request.getOffset())
                 .limit(request.size())
                 .fetch();
@@ -86,16 +70,23 @@ public class SpecRepositoryAdapter implements SpecRepositoryPort, SpecRepository
         return queryFactory
                 .select(
                         new QSpecWithDetailsDto(
-                                spec,
-                                examDetail,
-                                specBookmark.isNotNull()
+                                spec.name,
+                                spec.issuingOrganization,
+                                spec.certificationType,
+                                specBookmark.isNotNull(),
+                                examDetail.preparation,
+                                examDetail.eligibility,
+                                examDetail.examStructure,
+                                examDetail.passingCriteria
                         )
                 )
                 .from(spec)
                 .leftJoin(specBookmark)
                 .on(specBookmark.spec.id.eq(spec.id))
+                .leftJoin(exam)
+                .on(exam.id.eq(spec.latestExam.id))
                 .leftJoin(examDetail)
-                .on(examDetail.spec.id.eq(spec.id))
+                .on(examDetail.id.eq(exam.examDetail.id))
                 .fetchOne();
     }
 
