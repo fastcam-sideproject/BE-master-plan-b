d15488c5faab4f40195709d7112a5083e13559be
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-03-08 21:07:51 +0900

    refactor: Projection 용도의 DTO, 다시 레코드로 변경

--- src/main/java/com/example/masterplanbbe/domain/spec/dto/SpecWithDetailsDto.java
+++ src/main/java/com/example/masterplanbbe/domain/spec/dto/SpecWithDetailsDto.java
@@ -2,40 +2,15 @@ package com.example.masterplanbbe.domain.spec.dto;
 
 import com.example.masterplanbbe.domain.exam.enums.CertificationType;
 import com.querydsl.core.annotations.QueryProjection;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
-@Getter
-public class SpecWithDetailsDto {
-    private final String name;
-    private final String issuingOrganization;
-    private final CertificationType certificationType;
-    private final Boolean isBookmarked;
-    private final String preparation;
-    private final String eligibility;
-    private final String examStructure;
-    private final String passingCriteria;
-
-    @QueryProjection
-    public SpecWithDetailsDto(
-            String name,
-            String issuingOrganization,
-            CertificationType certificationType,
-            Boolean isBookmarked,
-            String preparation,
-            String eligibility,
-            String examStructure,
-            String passingCriteria
-    ) {
-        this.name = name;
-        this.issuingOrganization = issuingOrganization;
-        this.certificationType = certificationType;
-        this.isBookmarked = isBookmarked;
-        this.preparation = preparation;
-        this.eligibility = eligibility;
-        this.examStructure = examStructure;
-        this.passingCriteria = passingCriteria;
-    }
+public @QueryProjection record SpecWithDetailsDto (
+    String name,
+    String issuingOrganization,
+    CertificationType certificationType,
+    Boolean isBookmarked,
+    String preparation,
+    String eligibility,
+    String examStructure,
+    String passingCriteria
+) {
 }
