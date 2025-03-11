0edfb5440c9ff3ff09a51b1b4dfec421577dfe16
Jeong TaeHee <th1563@naver.com>
2025-03-12 02:52:49 +0900

    refactor: customObjectMapper에 @Primary 어노테이션 추가

--- src/main/java/com/example/masterplanbbe/common/config/JacksonConfig.java
+++ src/main/java/com/example/masterplanbbe/common/config/JacksonConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import java.time.LocalTime;
@@ -12,6 +13,7 @@ import java.time.LocalTime;
 @Configuration
 public class JacksonConfig {
     @Bean
+    @Primary
     public ObjectMapper customObjectMapper(Jackson2ObjectMapperBuilder builder) {
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
         SimpleModule simpleModule = new SimpleModule();
