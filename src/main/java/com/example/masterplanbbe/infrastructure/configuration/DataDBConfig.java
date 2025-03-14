37884c6c7851bcc99a978e2c0ab829d0516f785b
kimD0ngjun <chickentasty0112@gmail.com>
2025-03-15 01:20:46 +0900

    feat: 스프링 배치 설정 클래스 다시 추가

---
+++ src/main/java/com/example/masterplanbbe/infrastructure/configuration/DataDBConfig.java
@@ -0,0 +1,74 @@
+package com.example.masterplanbbe.infrastructure.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = {
+                "com.example.masterplanbbe.domain.repository",
+                "com.example.masterplanbbe.infrastructure.repository"
+        },
+        entityManagerFactoryRef = "dataEntityManager",
+        transactionManagerRef = "dataTransactionManager"
+)
+public class DataDBConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource-service")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean dataEntityManager() {
+
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setPackagesToScan(
+                "com.example.masterplanbbe.domain.entity"
+        );
+        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", "update");
+//        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+
+
+        factoryBean.setJpaPropertyMap(properties);
+        return factoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager dataTransactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(dataEntityManager().getObject());
+
+        return transactionManager;
+    }
+
+    @Bean
+    public PlatformTransactionManager jdbcTransactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
