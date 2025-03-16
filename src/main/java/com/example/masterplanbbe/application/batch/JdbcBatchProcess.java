9b53ffb9d94df76e4e5b65694d93514437786ef9
kimD0ngjun <chickentasty0112@gmail.com>
2025-03-17 00:39:57 +0900

    fix: TaskLet 단위로 작업 수정

--- src/main/java/com/example/masterplanbbe/application/batch/JdbcBatchProcess.java
+++ src/main/java/com/example/masterplanbbe/application/batch/JdbcBatchProcess.java
@@ -8,10 +8,13 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
@@ -23,6 +26,7 @@ public class JdbcBatchProcess {
 
     private final JobRepository jobRepository;
     private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
     private final PlatformTransactionManager transactionManager;
 
     @Bean
@@ -39,8 +43,9 @@ public class JdbcBatchProcess {
         log.info("Step 1: 기존 추천점수 구 추천점수 필드로 업데이트");
 
         return new StepBuilder("firstStep", jobRepository)
-                .<Recommendation, Recommendation>chunk(10, transactionManager)
-                .writer(updateOldScoreWriter())
+//                .<Recommendation, Recommendation>chunk(10, transactionManager)
+//                .writer(updateOldScoreWriter())
+                .tasklet(updateOldScoreTasklet(), transactionManager)
                 .build();
     }
 
@@ -48,11 +53,13 @@ public class JdbcBatchProcess {
      * 기존의 신규 추천점수를 구 추천점수로 업데이트하는 Writer
      */
     @Bean
-    public JdbcBatchItemWriter<Recommendation> updateOldScoreWriter() {
-        return new JdbcBatchItemWriterBuilder<Recommendation>()
-                .dataSource(dataSource)
-                .sql("UPDATE recommendations SET old_score = :newScore WHERE id = :id")
-                .beanMapped() // Java 객체 필드를 SQL 파라미터로 자동 매핑
-                .build();
+    public Tasklet updateOldScoreTasklet() {
+        return (contribution, chunkContext) -> {
+            // SQL 쿼리로 기존의 신규 추천점수를 구 추천점수로 업데이트
+            int updatedRows = jdbcTemplate.update("UPDATE recommendations SET old_score = new_score");
+            log.info("추천 점수 업데이트 완료, {}개의 레코드 업데이트", updatedRows);
+
+            return RepeatStatus.FINISHED;  // 작업 완료 후 종료
+        };
     }
 }
--- src/main/java/com/example/masterplanbbe/domain/repository/RecommendationRepository.java
+++ src/main/java/com/example/masterplanbbe/domain/repository/RecommendationRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecommendationRepository extends JpaRepository<Recommendation, Long>, CustomRecommendationRepository {
+public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
 }
