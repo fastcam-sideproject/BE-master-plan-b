be2a74ed6c58692d48fe217323fb1becd10c9b89
Nano2998 <gytjs2998@naver.com>
2025-03-16 19:56:08 +0900

    #118 fix: ssl 수정

--- src/main/java/com/example/masterplanbbe/infrastructure/configuration/RedisConfig.java
+++ src/main/java/com/example/masterplanbbe/infrastructure/configuration/RedisConfig.java
@@ -53,7 +53,7 @@ public class RedisConfig {
                 .clientOptions(clientoptions)
                 .commandTimeout(Duration.ofMinutes(1))
                 .shutdownTimeout(Duration.ZERO)
-                .useSsl()
+//                .useSsl()
                 .build();
 
         return new LettuceConnectionFactory(redisConfiguration, lettuceClientConfiguration);
