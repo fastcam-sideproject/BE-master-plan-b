d003e1fb408c9ead07a0da0adc7ee502fddfeabe
kimD0ngjun <chickentasty0112@gmail.com>
2025-03-17 23:02:05 +0900

    fix: 테이블명 추가

--- src/main/java/com/example/masterplanbbe/domain/entity/Comment.java
+++ src/main/java/com/example/masterplanbbe/domain/entity/Comment.java
@@ -4,8 +4,10 @@ import com.example.masterplanbbe.domain.common.FullAuditEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.*;
 
+@Table(name = "exam_comments")
 @Entity
 @Getter
 @Builder
