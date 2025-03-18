52d9154e943f00f7b4c6a091ff5d5842e07d75e9
Reddyong <d0209hong@naver.com>
2025-03-18 23:04:55 +0900

    #74 - feat : 학습 기록 전체 조회 기능 db 접근 로직 작성.
    * Querydsl을 사용하여 전체 조회 기능 구현.

--- src/main/java/com/example/masterplanbbe/infrastructure/repository/StudyLogRepositoryAdapter.java
+++ src/main/java/com/example/masterplanbbe/infrastructure/repository/StudyLogRepositoryAdapter.java
@@ -3,9 +3,16 @@ package com.example.masterplanbbe.infrastructure.repository;
 import com.example.masterplanbbe.domain.entity.StudyLog;
 import com.example.masterplanbbe.domain.repository.StudyLogRepository;
 import com.example.masterplanbbe.domain.repository.StudyLogRepositoryPort;
+import com.example.masterplanbbe.presentation.response.StudyLogResponse;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+import static com.example.masterplanbbe.domain.entity.QStudyLog.studyLog;
 import static com.example.masterplanbbe.infrastructure.exception.ErrorCode.NOT_FOUND_STUDY_LOG;
 import static com.example.masterplanbbe.infrastructure.exception.GlobalException.NotFoundException;
 
@@ -14,6 +21,7 @@ import static com.example.masterplanbbe.infrastructure.exception.GlobalException
 public class StudyLogRepositoryAdapter implements StudyLogRepositoryPort {
 
     private final StudyLogRepository studyLogRepository;
+    private final JPAQueryFactory jpaQueryFactory;
 
     @Override
     public StudyLog save(StudyLog studyLog) {
@@ -23,5 +31,32 @@ public class StudyLogRepositoryAdapter implements StudyLogRepositoryPort {
     @Override
     public StudyLog findByIdAndMemberId(Long id, String memberId) {
         return studyLogRepository.findByIdAndMemberEmail(id, memberId).orElseThrow(() -> new NotFoundException(NOT_FOUND_STUDY_LOG));
+    }
+
+    @Override
+    public List<StudyLogResponse> findAllStudyLogByYearAndMonthAndMemberId(Integer year, Integer month, String memberId) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (year != null) {
+            builder.and(studyLog.studyDate.year().eq(year));
+        }
+
+        if (month != null) {
+            builder.and(studyLog.studyDate.month().eq(month));
+        }
+
+        builder.and(studyLog.member.email.eq(memberId));
+
+        return jpaQueryFactory
+                .select(Projections.constructor(StudyLogResponse.class,
+                        studyLog.id,
+                        studyLog.studyDate,
+                        studyLog.totalStudyTimes,
+                        studyLog.content,
+                        studyLog.inputSource))
+                .from(studyLog)
+                .where(builder)
+                .fetch();
+
     }
 }
