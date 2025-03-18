faf632ce9561fc0faf38a638ddb2454773459fa1
Jeong TaeHee <th1563@naver.com>
2025-03-18 19:20:33 +0900

    refactor: Redis와 MySQL에서 채팅 메시지 조회 방식 분리 및 유효성 검사 추가

--- src/main/java/com/example/masterplanbbe/presentation/controller/ChatRestController.java
+++ src/main/java/com/example/masterplanbbe/presentation/controller/ChatRestController.java
@@ -4,9 +4,12 @@ import com.example.masterplanbbe.application.service.ChatService;
 import com.example.masterplanbbe.domain.enums.MemberRoleEnum;
 import com.example.masterplanbbe.infrastructure.security.jwt.JwtService;
 import com.example.masterplanbbe.presentation.response.ChatResponse;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,21 +24,19 @@ public class ChatRestController {
     private final JwtService jwtService;
 
     @GetMapping("/recent")
-    public ResponseEntity<?> getRecentMessages(@RequestParam Long specId,
-                                               @RequestParam(defaultValue = "50") int size) {
-        List<ChatResponse> recentMessages = chatService.getRecentMessages(specId, size);
+    public ResponseEntity<?> getRecentChatsFromRedis(@RequestParam Long specId,
+                                                     @RequestParam(defaultValue = "50") int size) {
+        List<ChatResponse> recentMessages = chatService.getRecentChatsFromRedis(specId, size);
         return ResponseEntity.ok().body(recentMessages);
     }
 
     @GetMapping
-    public ResponseEntity<?> getChatMessage(@RequestParam Long lastChatId,
-                                            @RequestParam Long specId,
-                                            @RequestParam(defaultValue = "50") int size) {
-        if (size <= 0) {
-            return ResponseEntity.badRequest().body("사이즈는 0 보다 커야합니다.");
-        }
-        Pageable pageable = PageRequest.of(0, Math.min(size, 100)); // 최대 사이즈 100으로 제한
-        return ResponseEntity.ok().body(chatService.getChatMessage(lastChatId, specId, pageable));
+    public ResponseEntity<?> getChatsFromMySQL(@RequestParam Long lastChatId,
+                                               @RequestParam Long specId,
+                                               @RequestParam(defaultValue = "50") @Min(1) @Max(100) int size) {
+        Pageable pageable = PageRequest.of(0, size);
+        Slice<ChatResponse> chatMessages = chatService.getChatMessage(lastChatId, specId, pageable);
+        return ResponseEntity.ok().body(chatMessages);
     }
 
     @DeleteMapping
@@ -45,7 +46,7 @@ public class ChatRestController {
                                         @RequestHeader("Authorization") String token) {
         MemberRoleEnum role = jwtService.getRoleFromAccessToken(token);
         boolean deleted = chatService.deleteChat(specId, chatId, memberId, role);
-        if(!deleted) {
+        if (!deleted) {
             //삭제 실패
         }
         return ResponseEntity.ok().body("채팅 메시지가 성공적으로 삭제되었습니다.");
