1653054ef8d2834eb0a532028a3498c5248c63bc
dfdrdodm95 <dfdrdodm95@gmail.com>
2025-03-26 15:04:19 +0900

    chore: webClient 의존성 추가

---
+++ src/main/java/com/example/masterplanbbe/infrastructure/util/ExamApiUtil.java
@@ -0,0 +1,10 @@
+package com.example.masterplanbbe.infrastructure.util;
+
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class ExamApiUtil {
+
+    private WebClient webClient;
+
+
+}
